3ec77128deb84e5ac2e6e581905b74892ca1903e
Zait06 <zait-hdez@hotmail.com>
2020-11-13 20:26:17 -0600

    dartca

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">DARTCA</span>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
